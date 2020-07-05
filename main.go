441aaa3fcc9511da5ada63cddbc44d72edc27ce4
Alvar Penning <post@0x21.biz>
2020-07-05 17:57:53 +0200

    MultiAction: Create Morse code based keyboard

--- main.go
+++ main.go
@@ -18,22 +18,29 @@ func main() {
 		panic(err)
 	}
 
-	micMuteAction := modes.NewCommandAction("amixer -c 0 set Capture toggle")
-	kbdSpaceAction, kbdErr := modes.NewKeyboardPressAction([]int{57})
-	if kbdErr != nil {
-		panic(kbdErr)
-	}
+	/*
+		micMuteAction := modes.NewCommandAction("amixer -c 0 set Capture toggle")
+		kbdSpaceAction, kbdErr := modes.NewKeyboardPressAction([]int{57})
+		if kbdErr != nil {
+			panic(kbdErr)
+		}
 
-	actionMap := map[string]modes.Action{
-		".": micMuteAction,
-		"_": kbdSpaceAction,
-	}
+		actionMap := map[string]modes.Action{
+			".": micMuteAction,
+			"_": kbdSpaceAction,
+		}
+	*/
 
-	multiAction := modes.NewMultiAction(signaler.Chan(), actionMap, samplingRate, morseMaxDotDuration, morseMinIdleDuration)
+	// multiAction := modes.NewMultiAction(signaler.Chan(), actionMap, samplingRate, morseMaxDotDuration, morseMinIdleDuration)
 	// trigger := modes.NewTrigger(signaler.Chan(), kbdSpaceAction, 500*time.Millisecond)
 
+	morseAction, morseErr := modes.NewMorseKeyboard(signaler.Chan(), samplingRate, morseMaxDotDuration, morseMinIdleDuration)
+	if morseErr != nil {
+		panic(morseErr)
+	}
+
 	go func() {
-		for err := range multiAction.Errors() {
+		for err := range morseAction.Errors() {
 			panic(err)
 		}
 	}()
@@ -41,7 +48,7 @@ func main() {
 	time.Sleep(30 * time.Second)
 	fmt.Println("Closing down..")
 
-	if err := multiAction.Close(); err != nil {
+	if err := morseAction.Close(); err != nil {
 		panic(err)
 	}
 	if err := signaler.Close(); err != nil {
--- modes/multi_action.go
+++ modes/multi_action.go
@@ -5,6 +5,8 @@ import (
 	"time"
 
 	"github.com/geistesk/pedal/pedal"
+
+	"github.com/micmonay/keybd_event"
 )
 
 // MultiAction performs different action based on a Morse code input.
@@ -36,6 +38,60 @@ func NewMultiAction(signalChan chan interface{}, actions map[string]Action, samp
 
 	go multiAction.worker()
 
+	return
+}
+
+func NewMorseKeyboard(signalChan chan interface{}, samplingRate, maxDotDuration, minIdleDuration time.Duration) (multiAction *MultiAction, err error) {
+	morseMap := map[string]int{
+		"._":    keybd_event.VK_A,
+		"_...":  keybd_event.VK_B,
+		"_._.":  keybd_event.VK_C,
+		"_..":   keybd_event.VK_D,
+		".":     keybd_event.VK_E,
+		".._.":  keybd_event.VK_F,
+		"__.":   keybd_event.VK_G,
+		"....":  keybd_event.VK_H,
+		"..":    keybd_event.VK_I,
+		".___":  keybd_event.VK_J,
+		"_._":   keybd_event.VK_K,
+		"._..":  keybd_event.VK_L,
+		"__":    keybd_event.VK_M,
+		"_.":    keybd_event.VK_N,
+		"___":   keybd_event.VK_O,
+		".__.":  keybd_event.VK_P,
+		"__._":  keybd_event.VK_Q,
+		"._.":   keybd_event.VK_R,
+		"...":   keybd_event.VK_S,
+		"_":     keybd_event.VK_T,
+		".._":   keybd_event.VK_U,
+		"..._":  keybd_event.VK_V,
+		".__":   keybd_event.VK_W,
+		"_.._":  keybd_event.VK_X,
+		"_.__":  keybd_event.VK_Y,
+		"__..":  keybd_event.VK_Z,
+		".____": keybd_event.VK_1,
+		"..___": keybd_event.VK_2,
+		"...__": keybd_event.VK_3,
+		"...._": keybd_event.VK_4,
+		".....": keybd_event.VK_5,
+		"_....": keybd_event.VK_6,
+		"__...": keybd_event.VK_7,
+		"___..": keybd_event.VK_8,
+		"____.": keybd_event.VK_9,
+		"_____": keybd_event.VK_0,
+	}
+
+	actions := map[string]Action{}
+	for k, v := range morseMap {
+		if kbdAction, kbdErr := NewKeyboardPressAction([]int{v}); kbdErr != nil {
+			err = kbdErr
+			return
+		} else {
+			actions[k] = kbdAction
+		}
+	}
+
+	multiAction = NewMultiAction(signalChan, actions, samplingRate, maxDotDuration, minIdleDuration)
 	return
 }
 
